d7ea1667586af27a29ebb045f40d6165f265f435
toto271 <82321634+toto271@users.noreply.github.com>
2021-04-29 22:04:38 +0900

    Script-test
    発展に変更

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,33 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Boss
+{
+    private int mp = 53;
+
+    public void Magic()
+    {
+        while (true)
+        {
+
+
+            if (this.mp >= 5)
+            {
+                this.mp -= 5;
+
+                Debug.Log("魔法攻撃をした。残りMPは" + this.mp + ".");
+
+                continue;
+            }
+
+            else
+            {
+                Debug.Log("MPが足りないため魔法が使えない");
+
+                break;
+            }
+        }
+
+
+    }
+}
 public class Test : MonoBehaviour
 {
 
     void Start()
     {
-        int[] points = { 50, 100, 150, 200, 250 };
 
-        for (int i = 0; i < points.Length; i++)
-        {
 
-            Debug.Log(points[i]);
-        }
-        for (int i = 4; i >= 0; i--)
-        {
+        Boss lastboss = new Boss();
 
-            Debug.Log(points[i]);
-        }
+        lastboss.Magic();
 
 
 
-    }    
 
-
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
