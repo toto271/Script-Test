fb7c3e488db8f616bb8b645f136f379a64ee1e90
toto271 <82321634+toto271@users.noreply.github.com>
2021-04-29 16:47:18 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] points = { 50, 100, 150, 200, 250 };
+
+        for (int i = 0; i < points.Length; i++)
+        {
+
+            Debug.Log(points[i]);
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+
+            Debug.Log(points[i]);
+        }
+
+
+
+    }    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
